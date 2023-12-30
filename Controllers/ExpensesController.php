4d5a763b7e897d076a7ee70068744b8c974b0cf2
Poonam Magar <102014652+PoonamMagar123@users.noreply.github.com>
2023-12-30 22:15:38 +0530

    Add files via upload

--- Controllers/ExpensesController.php
+++ Controllers/ExpensesController.php
@@ -2,6 +2,8 @@
 
 use App\Models\Expense;
 
+use Mpdf\Mpdf;
+
 class ExpensesController extends BaseController
 {
     public function index()
@@ -49,53 +51,84 @@ class ExpensesController extends BaseController
 
 
     public function update($id)
-{   
-    $expenseModel = new Expense();
-
-    // Find the expense item by ID
-    $expenseItem = $expenseModel->find($id);
-
-    // Get the uploaded file
-    $file = $this->request->getFile('photo');
-
-    // Check if a file is uploaded and valid
-    if ($file && $file->isValid() && !$file->hasMoved()) {
-        // Delete the old image file
-        $oldImageName = $expenseItem['photo'];
-        if (file_exists("uploads/" . $oldImageName)) {
-            unlink("uploads/" . $oldImageName);
-        }
+    {   
+        $expenseModel = new Expense();
+        $expenseItem = $expenseModel->find($id);
         
-        // Move the new file to the "uploads" directory
-        $imageName = $file->getRandomName();
-        $file->move('uploads/', $imageName);
-    } else {
-        // If no file is uploaded or the file is not valid, keep the existing photo
-        $imageName = $expenseItem['photo'];
+        $oldImageName = $expenseItem['photo'];
+        $file = $this->request->getFile('photo');
+
+        // Check if a file is uploaded and valid
+        if ($file->isValid() && !$file->hasMoved()) 
+        {
+            // Delete the old image file
+            if (file_exists("uploads/" . $oldImageName)) {
+                unlink("uploads/" . $oldImageName);
+            }
+            
+            // Move the new file to the "uploads" directory
+            $imageName = $file->getRandomName();
+            $file->move('uploads/', $imageName);
+        } 
+        else 
+        {
+            // If no file is uploaded or the file is not valid, keep the existing photo
+            $imageName = $oldImageName;
+        }
+
+        // Update the other fields in the database
+        $data = [
+            'name' => $this->request->getPost('name'),
+            'phone' => $this->request->getPost('phone'),
+            'photo' => $imageName,
+            'amount' => $this->request->getPost('amount'),
+            'expense' => $this->request->getPost('expense'),
+            'total_expense' => $this->request->getPost('total_expense')
+        ];
+
+        // Update the record in the database
+        $expenseModel->update($id, $data);
+
+        return redirect()->to(base_url('expense'))->with('status', 'Expense Details Updated successfully!...');
     }
-
-    // Update the other fields in the database
-    $data = [
-        'name' => $this->request->getPost('name'),
-        'phone' => $this->request->getPost('phone'),
-        'photo' => $imageName,
-        'amount' => $this->request->getPost('amount'),
-        'expense' => $this->request->getPost('expense'),
-        'total_expense' => $this->request->getPost('total_expense')
-    ];
-
-    // Update the record in the database
-    $expenseModel->update($id, $data);
-
-    return redirect()->to(base_url('expense'))->with('status', 'Expense Details Updated successfully!...');
-}
-
 
     public function delete($id)
     {   
         $expensemodel = new Expense();
+
+        $data = $expensemodel->find($id);
+        $imagefile = $data['photo'];
+        if(file_exists('uploads/'. $imagefile))
+        {
+            unlink('uploads/'. $imagefile);
+        }
         $expensemodel->delete($id);
         return redirect()->to(base_url('expense'))->with('status', 'Expense Details Deleted successfully!...');
     }
+
+    public function download($id)
+    {
+        $expensemodel = new Expense();
+
+        // Fetch data for the selected expense (you might need to adjust this based on your model)
+        $expense = $expensemodel->find($id);
+
+        if ($expense) {
+            // Generate PDF content (use the appropriate view for your data)
+            $pdfContent = view('pdf_template', ['expenses' => [$expense]]);
+
+            // Create a response object with the PDF content
+            $response = service('response');
+            $response->setHeader('Content-Type', 'application/pdf');
+            $response->setHeader('Content-Disposition', 'attachment; filename=ExpenseData.pdf');
+            $response->setBody($pdfContent);
+
+            return $response;
+        } else {
+            // Handle the case where the expense is not found
+            return redirect()->to(base_url('expense'))->with('error', 'Expense not found!');
+        }
+    }
+
 }
 ?>
